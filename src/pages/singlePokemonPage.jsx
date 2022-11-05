ea9ac43b9461d43924fd0ded1af168cedf507f4e
basagoitiam <marika.basagoitia@gmail.com>
2022-11-05 13:57:34 -0400

    added more styles for single page

--- src/pages/singlePokemonPage.jsx
+++ src/pages/singlePokemonPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { BrowserRouter, NavLink, Link, useParams } from "react-router-dom";
 import 'font-awesome/css/font-awesome.min.css';
 
 
@@ -8,6 +8,8 @@ const API_URL = "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/maste
 function SinglePokemonPage () {
     const [singlePokemon, setSinglePokemon] = useState("");
     const [hasLoaded, setHasLoaded] = useState(false);
+    const [prevEvolution, setPrevEvolution] = useState("");
+    const [nextEvolution, setNextEvolution] = useState("");
     const { name } = useParams();
 
 
@@ -26,43 +28,37 @@ function SinglePokemonPage () {
         fetchPokemon();
       }, []);
   
+      useEffect(() => {
+        if (singlePokemon.hasOwnProperty("prev_evolution")) {
+          setPrevEvolution(singlePokemon.prev_evolution[singlePokemon.prev_evolution.length-1].name);
+        }
+  
+        if (singlePokemon.hasOwnProperty("next_evolution")) {
+          setNextEvolution(singlePokemon.next_evolution[0].name);
+        }
+      }, [singlePokemon])
+
       if (!hasLoaded) {
           return <p>Loading...</p>
       }
 
-      let previousEvolution = "";
-      let nextEvolution = "";
-
-
-      //i think this needs to be controlled by state
-      
-      if (singlePokemon.hasOwnProperty("prev_evolution")) {
-        previousEvolution = singlePokemon.prev_evolution[singlePokemon.prev_evolution.length-1].name;
-      } else {
-        document.querySelector(".prev-evolution").classList.add("not-visible");
-      }
-
-      if (singlePokemon.hasOwnProperty("next_evolution")) {
-        nextEvolution = singlePokemon.next_evolution[0].name;
-      } else {
-        document.querySelector(".next-evolution").classList.add("not-visible");
-      }
-
-
       return (
         <div style={{textAlign: "center", display: "flex", justifyContent: "space-evenly", alignItems: "center", minHeight: "100vh"}}>
           <div className="arrow-group prev-evolution">
-            <i className="fa-solid fa-arrow-left arrow left"></i>
-            <span className="previous-evolution-text">{previousEvolution}</span>
+            <NavLink to={`/${prevEvolution}`}>
+              <i className="fa-solid fa-arrow-left arrow left"></i>
+              <span className="previous-evolution-text">{prevEvolution}</span>
+            </NavLink>
           </div>
           <div id="img-title-wrapper">
-            {console.log(singlePokemon)};
             <img id="single-pokemon-img" src={singlePokemon.img} alt={`Image of ${singlePokemon.name}`}></img>
             <h1 id="single-pokemon-title">{singlePokemon.name}</h1>
           </div>
           <div className="arrow-group next-evolution">
+          <NavLink to={`/${nextEvolution}`}>
             <i className="fa-solid fa-arrow-right arrow right"></i>
             <span className="previous-evolution-text">{nextEvolution}</span>
+          </NavLink>
           </div>
         </div>
       )
